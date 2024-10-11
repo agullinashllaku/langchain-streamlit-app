1d6bddb2413e96cec734858af2be3c58a69d24de
Agullina Shllaku <agullina.s@gmail.com>
2024-10-12 01:29:52 +0200

    Import added

--- query_data.py
+++ query_data.py
@@ -8,10 +8,12 @@ from extract_MD import url_dict
 import sys
 import os
 import importlib.util
+import openai
 
 
 # Load environment variables
 load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
